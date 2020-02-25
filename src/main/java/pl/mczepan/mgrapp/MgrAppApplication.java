3fd06c99f66b0eddb0fbe1ee911b0477980d7b03
Maciej Czepan <m.czepan@it-learn.pl>
2020-02-25 12:37:07 +0100

    set up project

---
+++ src/main/java/pl/mczepan/mgrapp/MgrAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mczepan.mgrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgrAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgrAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mczepan/mgrapp/MgrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mczepan.mgrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgrAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
